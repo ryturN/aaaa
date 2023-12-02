5175603a06334fa8132fb0811c81ecf3c35c5dca
Asty Yuliani <100658838+astyyuliani@users.noreply.github.com>
2023-12-02 12:46:54 +0700

    Cloud Computing

--- db/firebase.js
+++ db/firebase.js
@@ -3,12 +3,7 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./service-account.json");
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSd-pH20VU1O1bd6sPXDGTbKmALIqaBIc",
-  authDomain: "testcapstone-bd06c.firebaseapp.com",
-  projectId: "testcapstone-bd06c",
-  storageBucket: "testcapstone-bd06c.appspot.com",
-  messagingSenderId: "225869817417",
-  appId: "1:225869817417:web:66dffe5c6818b427a24198"
+//
 };
 
 admin.initializeApp({
